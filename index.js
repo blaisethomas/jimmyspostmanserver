37ab7658fc218dcb4a54ed59fb774a5d12e762b8
Blaise Thomas <blaisegthomas@gmail.com>
2016-10-07 12:49:24 -0700

    PORT

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var request = require ('request');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000 ;
 
 var app = express();
 app.use(express.static('public'));
